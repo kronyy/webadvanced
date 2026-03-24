edc8e48cd79d709e15da37941962c82a77937a5b
kronyy <kronbekolli12@gmail.com>
2026-03-24 19:19:37 +0100

    module 26

---
+++ module 26/main.js
@@ -0,0 +1,54 @@
+ console.log("hello");
+
+ var M238=65*67
+
+ console.log(M238)
+
+ var a=55
+ var b=1112
+ var c=a+b
+ console.log(c)
+
+ var w=55
+ var q=1112
+ var c=w-q
+ console.log(c)
+
+ var l=45455454
+ var u=5484456
+ var k=l/u
+ console.log(k)
+
+ var e=55
+ var g=1112
+ var n=e%g
+ console.log(n)
+
+ var s=55
+ s++;
+ console.log(s)
+
+  var sh=55
+ sh--;
+ console.log(sh)
+
+  var z=55
+ var f=z**2
+ console.log(z)
+
+ var y=15
+  y+=5
+ console.log(y)
+
+
+var b1=5;
+var b6="88";
+
+console.log(b1 > b6)
+console.log(b1 < b6)
+console.log(b1 === b6)
+
+var name="filan"
+var surname="fisteku"
+
+console.log(name+surname)
